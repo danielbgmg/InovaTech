daaf159ea2e1f55486678cd12947792314923a1f
Daniel Gomes <96587031+danielbgmg@users.noreply.github.com>
2024-07-24 21:20:34 -0300

    feat: Criação dos arquivos base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer/Footer"
+import Header from "./Header/Header"
+import Home from "./Home/Home"
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
