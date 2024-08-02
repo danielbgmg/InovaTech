cccc6dca8f986496f505aa9f61044b07f802e290
Daniel Gomes <daniel167t@gmail.com>
2024-08-02 16:52:17 -0300

    feat: criação dos posts

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -3,42 +3,39 @@ import HeaderSection from '../../components/HeaderSection/HeaderSection';
 
 export default function about() {
   return (
-    <>
-    
-      <div className="main">
-        <HeaderSection 
-        title="Sobre Nós"
-        />
+    <div className="main">
+      <HeaderSection 
+      title="Sobre Nós"
+      />
 
-        <section>
-          <div className="header-text">
-            <div className="about-header">
+      <section>
+        <div className="header-text">
+          <div className="about-header">
+            <div>
+              <h2 className="about-subTitle">Sejam Bem Vindos à</h2>
               <div>
-                <h2 className="about-subTitle">Sejam Bem Vindos à</h2>
-                <div>
-                  <img src="/src/assets/img/logo-name.png" alt="" />
-                </div>
+                <img src="/src/assets/img/logo-name.png" alt="" />
               </div>
-              <p>
-                Na InovaTech, nossa paixão por inovação e tecnologia nos impulsiona a entregar soluções de ponta que transformam ideias em realidade. Fundada por entusiastas da tecnologia, nossa missão é capacitar indivíduos e empresas com ferramentas e conhecimentos essenciais para prosperar no mundo digital em constante evolução.
-              </p>
             </div>
-            <img src="/src/assets/img/logo-icon.png" alt="" className="logo-g"/>
-          </div>
-          <div>
             <p>
-              Nossa equipe é composta por especialistas talentosos em diversas áreas, incluindo desenvolvimento back-end, front-end e design UX/UI. Juntos, combinamos criatividade, expertise técnica e uma abordagem centrada no usuário para desenvolver produtos que não apenas atendem às necessidades atuais, mas também antecipam os desafios futuros.
-            </p>
-            <p>
-              Valorizamos a educação contínua e acreditamos que o aprendizado é a chave para o progresso. Por isso, oferecemos uma ampla gama de cursos e recursos educativos, projetados para aprimorar suas habilidades e mantê-lo à frente das tendências tecnológicas.
-            </p>
-            <p>
-              Na InovaTech, cada projeto é uma nova oportunidade de inovar e exceder expectativas. Estamos comprometidos em criar experiências excepcionais e soluções tecnológicas que impulsionam o sucesso de nossos clientes e parceiros. Junte-se a nós nessa jornada de transformação e descubra como podemos ajudar a levar suas ideias ao próximo nível.
+              Na InovaTech, nossa paixão por inovação e tecnologia nos impulsiona a entregar soluções de ponta que transformam ideias em realidade. Fundada por entusiastas da tecnologia, nossa missão é capacitar indivíduos e empresas com ferramentas e conhecimentos essenciais para prosperar no mundo digital em constante evolução.
             </p>
           </div>
-        </section>
+          <img src="/src/assets/img/logo-icon.png" alt="" className="logo-g"/>
+        </div>
+        <div>
+          <p>
+            Nossa equipe é composta por especialistas talentosos em diversas áreas, incluindo desenvolvimento back-end, front-end e design UX/UI. Juntos, combinamos criatividade, expertise técnica e uma abordagem centrada no usuário para desenvolver produtos que não apenas atendem às necessidades atuais, mas também antecipam os desafios futuros.
+          </p>
+          <p>
+            Valorizamos a educação contínua e acreditamos que o aprendizado é a chave para o progresso. Por isso, oferecemos uma ampla gama de cursos e recursos educativos, projetados para aprimorar suas habilidades e mantê-lo à frente das tendências tecnológicas.
+          </p>
+          <p>
+            Na InovaTech, cada projeto é uma nova oportunidade de inovar e exceder expectativas. Estamos comprometidos em criar experiências excepcionais e soluções tecnológicas que impulsionam o sucesso de nossos clientes e parceiros. Junte-se a nós nessa jornada de transformação e descubra como podemos ajudar a levar suas ideias ao próximo nível.
+          </p>
+        </div>
+      </section>
 
-      </div>
-    </>
+    </div>
   );
 }
--- src/pages/Blog/blog.jsx
+++ src/pages/Blog/blog.jsx
@@ -1,8 +1,50 @@
 import "./_blog.scss";
 export default function blog() {
   return (
-    <div>
-      <h1>BLOG</h1>
+    <div className="main">
+      <div className="blog-header">
+        <div>
+          <h1 className="blog-title">BLOG</h1>    
+        </div>
+        <div className="blog-headerImgs">
+          <div className="blog-img1">
+            <img src="/src/assets/img/Blog-img1.png" alt="" />
+          </div>
+          <div className="blog-img2">
+            <img src="/src/assets/img/Blog-img2.png" alt="" />
+            <div>
+              <img src="/src/assets/img/Blog-img3.png" alt="" />
+              <img src="/src/assets/img/Blog-img4.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="blog-posts">
+        <div className="post-card">
+          <img src="/src/assets/img/post-img1.png" alt="" />
+          <div className="post-text">
+            <h2>Quais são os cinco pilares da Inteligência Emocional?</h2>
+            <p>Inteligência emocional é a capacidade de captar o mundo por meio dos estímulos. Praticá-la é transformador, pois a partir disso, é possível dominar suas emoções e usá-las a favor de si mesmo...Ler Mais
+            </p>
+          </div>
+        </div>
+        <div className="post-card">
+          <img src="/src/assets/img/post-img2.png" alt="" />
+          <div className="post-text">
+            <h2>Resiliência: A Habilidade Essencial para o Sucesso Pessoal e Profissional</h2>
+            <p>A resiliência é uma das competências mais importantes para quem deseja alcançar sucesso tanto na vida pessoal quanto na profissional. Em um mundo repleto de desafios e mudanças constantes, a capacidade de se ...Ler Mais
+            </p>
+          </div>
+        </div>
+        <div className="post-card">
+          <img src="/src/assets/img/post-img3.png" alt="" />
+          <div className="post-text">
+            <h2>Ambiente de trabalho tóxico: 7 sinais para você ficar alerta!</h2>
+            <p>Um ambiente de trabalho tóxico pode ser definido como aquele que afeta negativamente os trabalhadores de uma empresa, destacando que o resultado disso pode ser sentido dentro e fora dela. Ler mais
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
