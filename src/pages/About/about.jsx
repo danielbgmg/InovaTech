9721541c29d12e6e8f41873f0301ec32b42458a9
Martin Vitabar <vitabarmartin@gmail.com>
2024-08-02 19:26:50 -0300

    imagens importadas

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,12 +1,12 @@
 import "./_about.scss";
-import HeaderSection from '../../components/HeaderSection/HeaderSection';
+import HeaderSection from "../../components/HeaderSection/HeaderSection";
+import LogoName from "../../assets/img/logo-name.png";
+import LogoG from "../../assets/img/logo-icon.png";
 
 export default function about() {
   return (
     <div className="main">
-      <HeaderSection 
-      title="Sobre Nós"
-      />
+      <HeaderSection title="Sobre Nós" />
 
       <section>
         <div className="header-text">
@@ -14,28 +14,43 @@ export default function about() {
             <div>
               <h2 className="about-subTitle">Sejam Bem Vindos à</h2>
               <div>
-                <img src="/src/assets/img/logo-name.png" alt="" />
+                <img src={LogoName} alt="" />
               </div>
             </div>
             <p>
-              Na InovaTech, nossa paixão por inovação e tecnologia nos impulsiona a entregar soluções de ponta que transformam ideias em realidade. Fundada por entusiastas da tecnologia, nossa missão é capacitar indivíduos e empresas com ferramentas e conhecimentos essenciais para prosperar no mundo digital em constante evolução.
+              Na InovaTech, nossa paixão por inovação e tecnologia nos
+              impulsiona a entregar soluções de ponta que transformam ideias em
+              realidade. Fundada por entusiastas da tecnologia, nossa missão é
+              capacitar indivíduos e empresas com ferramentas e conhecimentos
+              essenciais para prosperar no mundo digital em constante evolução.
             </p>
           </div>
-          <img src="/src/assets/img/logo-icon.png" alt="" className="logo-g"/>
+          <img src={LogoG} alt="" className="logo-g" />
         </div>
         <div>
           <p>
-            Nossa equipe é composta por especialistas talentosos em diversas áreas, incluindo desenvolvimento back-end, front-end e design UX/UI. Juntos, combinamos criatividade, expertise técnica e uma abordagem centrada no usuário para desenvolver produtos que não apenas atendem às necessidades atuais, mas também antecipam os desafios futuros.
+            Nossa equipe é composta por especialistas talentosos em diversas
+            áreas, incluindo desenvolvimento back-end, front-end e design UX/UI.
+            Juntos, combinamos criatividade, expertise técnica e uma abordagem
+            centrada no usuário para desenvolver produtos que não apenas atendem
+            às necessidades atuais, mas também antecipam os desafios futuros.
           </p>
           <p>
-            Valorizamos a educação contínua e acreditamos que o aprendizado é a chave para o progresso. Por isso, oferecemos uma ampla gama de cursos e recursos educativos, projetados para aprimorar suas habilidades e mantê-lo à frente das tendências tecnológicas.
+            Valorizamos a educação contínua e acreditamos que o aprendizado é a
+            chave para o progresso. Por isso, oferecemos uma ampla gama de
+            cursos e recursos educativos, projetados para aprimorar suas
+            habilidades e mantê-lo à frente das tendências tecnológicas.
           </p>
           <p>
-            Na InovaTech, cada projeto é uma nova oportunidade de inovar e exceder expectativas. Estamos comprometidos em criar experiências excepcionais e soluções tecnológicas que impulsionam o sucesso de nossos clientes e parceiros. Junte-se a nós nessa jornada de transformação e descubra como podemos ajudar a levar suas ideias ao próximo nível.
+            Na InovaTech, cada projeto é uma nova oportunidade de inovar e
+            exceder expectativas. Estamos comprometidos em criar experiências
+            excepcionais e soluções tecnológicas que impulsionam o sucesso de
+            nossos clientes e parceiros. Junte-se a nós nessa jornada de
+            transformação e descubra como podemos ajudar a levar suas ideias ao
+            próximo nível.
           </p>
         </div>
       </section>
-
     </div>
   );
 }
--- src/pages/Blog/blog.jsx
+++ src/pages/Blog/blog.jsx
@@ -1,46 +1,70 @@
 import "./_blog.scss";
+import Image1 from "../../assets/img/blog-img1.png";
+import Image2 from "../../assets/img/blog-img2.png";
+import Image3 from "../../assets/img/blog-img3.png";
+import Image4 from "../../assets/img/blog-img4.png";
+import PostImg1 from "../../assets/img/post-img1.png";
+import PostImg2 from "../../assets/img/post-img2.png";
+import PostImg3 from "../../assets/img/post-img3.png";
 export default function blog() {
   return (
     <div className="main">
       <div className="blog-header">
         <div>
-          <h1 className="blog-title">BLOG</h1>    
+          <h1 className="blog-title">BLOG</h1>
         </div>
         <div className="blog-headerImgs">
           <div className="blog-img1">
-            <img src="/src/assets/img/Blog-img1.png" alt="" />
+            <img src={Image1} alt="" />
           </div>
           <div className="blog-img2">
-            <img src="/src/assets/img/Blog-img2.png" alt="" />
+            <img src={Image2} alt="" />
             <div>
-              <img src="/src/assets/img/Blog-img3.png" alt="" />
-              <img src="/src/assets/img/Blog-img4.png" alt="" />
+              <img src={Image3} alt="" />
+              <img src={Image4} alt="" />
             </div>
           </div>
         </div>
       </div>
       <section className="blog-posts">
         <div className="post-card">
-          <img src="/src/assets/img/post-img1.png" alt="" />
+          <img src={PostImg1} alt="" />
           <div className="post-text">
             <h2>Quais são os cinco pilares da Inteligência Emocional?</h2>
-            <p>Inteligência emocional é a capacidade de captar o mundo por meio dos estímulos. Praticá-la é transformador, pois a partir disso, é possível dominar suas emoções e usá-las a favor de si mesmo...Ler Mais
+            <p>
+              Inteligência emocional é a capacidade de captar o mundo por meio
+              dos estímulos. Praticá-la é transformador, pois a partir disso, é
+              possível dominar suas emoções e usá-las a favor de si mesmo...Ler
+              Mais
             </p>
           </div>
         </div>
         <div className="post-card">
-          <img src="/src/assets/img/post-img2.png" alt="" />
+          <img src={PostImg2} alt="" />
           <div className="post-text">
-            <h2>Resiliência: A Habilidade Essencial para o Sucesso Pessoal e Profissional</h2>
-            <p>A resiliência é uma das competências mais importantes para quem deseja alcançar sucesso tanto na vida pessoal quanto na profissional. Em um mundo repleto de desafios e mudanças constantes, a capacidade de se ...Ler Mais
+            <h2>
+              Resiliência: A Habilidade Essencial para o Sucesso Pessoal e
+              Profissional
+            </h2>
+            <p>
+              A resiliência é uma das competências mais importantes para quem
+              deseja alcançar sucesso tanto na vida pessoal quanto na
+              profissional. Em um mundo repleto de desafios e mudanças
+              constantes, a capacidade de se ...Ler Mais
             </p>
           </div>
         </div>
         <div className="post-card">
-          <img src="/src/assets/img/post-img3.png" alt="" />
+          <img src={PostImg3} alt="" />
           <div className="post-text">
-            <h2>Ambiente de trabalho tóxico: 7 sinais para você ficar alerta!</h2>
-            <p>Um ambiente de trabalho tóxico pode ser definido como aquele que afeta negativamente os trabalhadores de uma empresa, destacando que o resultado disso pode ser sentido dentro e fora dela. Ler mais
+            <h2>
+              Ambiente de trabalho tóxico: 7 sinais para você ficar alerta!
+            </h2>
+            <p>
+              Um ambiente de trabalho tóxico pode ser definido como aquele que
+              afeta negativamente os trabalhadores de uma empresa, destacando
+              que o resultado disso pode ser sentido dentro e fora dela. Ler
+              mais
             </p>
           </div>
         </div>
