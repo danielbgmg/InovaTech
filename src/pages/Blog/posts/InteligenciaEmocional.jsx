f00be3d5f585255697158e85a95e7de23e314b32
Daniel Gomes <daniel167t@gmail.com>
2024-08-09 11:00:10 -0300

    Atualizando os post do blog

---
+++ src/pages/Blog/posts/InteligenciaEmocional.jsx
@@ -0,0 +1,159 @@
+import Image from "../../../assets/img/post-img1.png";
+import "./_posts.scss";
+import { Link } from "react-router-dom";
+export default function InteligengiaEmocional() {
+  return (
+    <main className="main main-posts">
+        <div className="header-post">
+            <span className="blog-title">BLOG</span>
+            <img src={Image} alt="" />
+            <header className="post-title">
+              <h1>Quais são os cinco pilares da Inteligência Emocional?</h1>
+              <div>
+                  <span className="post-authorName">Pucs Online</span>
+                  <span className="separador">|</span>
+                  <span>26 JAN .2024</span>
+              </div>
+            </header>
+        </div>
+        <section>
+          <p>
+            Inteligência emocional é a capacidade de captar o mundo por meio dos
+            estímulos. Praticá-la é transformador, pois a partir disso, é
+            possível dominar suas emoções e usá-las a favor de si mesmo. Há uma
+            metáfora da neurociência que explica o cérebro humano dividido em
+            duas áreas: hemisfério esquerdo e hemisfério direito. O esquerdo
+            seria o lado racional, matemático, lógico, cognitivo, analista e
+            crítico. Já o direito, seria o lado da compreensão emocional do
+            mundo, responsável pela intuição, emoções e sentimentos. Apesar de
+            distintos, a inteligência emocional passa diretamente por esses dois
+            hemisférios. Desenvolver essa habilidade é um aprendizado
+            fundamental para a vida. Por meio do controle dos nossos impulsos e
+            sentimentos, podemos dominar sensações de raiva, tristeza ou
+            desânimo. Dessa forma, a partir do momento em que começamos a
+            perceber que somos movidos principalmente pelos sentimentos, e isto
+            acontece inconscientemente, é que identificamos a importância de
+            saber como a inteligência emocional é constituída.
+          </p>
+          <h2>Os cinco pilares da Inteligência Emocional</h2>
+          <p>
+            Entender os cinco pilares da Inteligência Emocional é o primeiro
+            passo para desenvolvê-la e praticá-la diariamente. A partir disso,
+            será possível ter maior controle das próprias emoções, facilitando a
+            construção de relações saudáveis e tomadas de decisões conscientes.
+            Essa lógica é de Daniel Goleman, psicólogo e jornalista científico
+            conhecido como o “pai da Inteligência Emocional”. É autor do livro
+            “Inteligência Emocional: a teoria revolucionária que redefine o que
+            é ser inteligente”, considerada a principal obra sobre o tema. A
+            seguir, conheça os cinco pilares da Inteligência Emocional,
+            determinantes para se ter êxito nos relacionamentos e até bem-estar
+            físico:
+          </p>
+          <h2>Conhecer as próprias emoções</h2>
+          <p>
+            Este princípio diz respeito ao autoconhecimento. Para praticá-lo,
+            você precisa analisar suas emoções e atos como resposta aos
+            estímulos recebidos. Quando você se sente frustrado, investigue se
+            essa uma resposta verdadeira ou se ela está escondendo um outro
+            sentimento. A ansiedade é outra sensação que pode ser responsável
+            por esconder outras emoções muito mais complexas. Por trás desses
+            sentimentos, pode existir medo do fracasso, receio de julgamentos,
+            temor de se apresentar em frente a um público. Reconhecer
+            verdadeiramente o que você está sentindo, a ponto de nomear
+            corretamente as suas emoções, é o primeiro passo fundamental para
+            desenvolver a Inteligência Emocional. Ou seja, é preciso conhecer as
+            emoções para poder gerenciá-las. No entanto, também é preciso estar
+            ciente do processo gradual que implica esse conhecimento e como ele
+            varia de pessoa para pessoa. A fim de facilitar o processo de
+            descoberta, um exercício simples e benéfico é anotar, ao fim de cada
+            dia, os sentimentos que você percebeu e como lidou com eles.
+          </p>
+          <h2>Controlar as emoções</h2>
+          <p>
+            O gerenciamento das emoções é feito a partir do conhecimento delas.
+            Após entender as suas emoções, é o momento de trabalhá-las. Ao
+            finalizar uma semana anotando os seus sentimentos e a forma como
+            lidou com eles, faça um balanço. Quais foram os sentimentos mais
+            frequentes? De que forma como você lidou com esses sentimentos?
+            Segundo Daniel Goleman, a consciência das emoções é fator essencial
+            para o desenvolvimento da inteligência do indivíduo. Ao controlar as
+            emoções, é importante entender a diferença existente entre dois
+            conceitos: autopercepção e heteropercepção. A autopercepção
+            refere-se ao que entendemos e percebemos. A heteropercepção diz
+            respeito ao modo como os outros enxergam a mesma situação. Muitas
+            vezes, através da primeira, achamos que nosso modo de agir é
+            assertivo. No entanto, pessoas ao nosso redor podem interpretar
+            nossas ações como as de uma pessoa ríspida. O que as pessoas
+            enxergam não é, necessariamente, o que você pensa. Sendo assim,
+            conhecer as possíveis percepções alheias também é fundamental para
+            trabalhar sua Inteligência Emocional. É por meio deste controle que
+            se domina a melhor forma de emitir mensagens e, assim, evitar
+            entendimentos distorcidos.
+          </p>
+          <h2>Automotivação</h2>
+          <p>
+            O domínio sobre a percepção lançada quando emitimos uma mensagem tem
+            relação com o gerenciamento das nossas emoções. Aprender a
+            controlá-las e racionalizar antes de tomar qualquer decisão, traz
+            grandes benefícios. É essa gestão que possibilita a diminuição de
+            conflitos interpessoais ou mesmo conflitos internos. Você, no
+            controle das suas emoções, tem um caminho muito mais tranquilo e
+            equilibrado em direção aos seus objetivos. Isto implica,
+            principalmente, na mudança de antigos padrões de comportamento. É
+            preciso acreditar que dá para mudar. Não existe o “eu sou assim
+            mesmo”. É neste momento que a automotivação mostra sua importância
+            como um dos pilares fundamentais da Inteligência Emocional. Ela é
+            fundamental para investir em uma mudança que possibilitará maior
+            crescimento no trabalho, nos relacionamentos e na vida. Lembre-se do
+            investimento na sua mudança e nos benefícios pessoais e
+            profissionais que o domínio da Inteligência Emocional trará.
+          </p>
+          <h2>Empatia</h2>
+          <p>
+            Colocar-se no lugar do outro. Isto é empatia. Tal qual a
+            Inteligência Emocional, a empatia é um conceito que tem ganhado
+            grande muito destaque. Mas como colocar-se no lugar do outro, de
+            maneira sensível e aberta, desprovida de julgamentos? A empatia
+            envolve muito mais do que validar e respeitar o sentimento do outro.
+            Ela diz respeito a se inserir no contexto em que o outro está e
+            entender a razão de aquele sentimento despertar determinada emoção
+            para o outro. É importante ter consciência que a empatia é uma
+            escolha, além de ser um processo que requer dedicação. Ela melhora
+            as relações humanas e possibilita ver as coisas pelo ângulo das
+            outras pessoas. Ter empatia, tornar-se mais habilidoso nas relações
+            e entender os desejos e motivações do outro são fatores fundamentais
+            para a vida em sociedade
+          </p>
+          <h2>Relacionar-se interpessoalmente</h2>
+          <p>
+            Sempre precisaremos do outro. Somos parte de um organismo maior, o
+            organismo social. Para se relacionar bem socialmente, é fundamental
+            ter o equilíbrio entre a empatia, o autocontrole a autoconsciência.
+            Conhecer e se relacionar com pessoas é conhecer possibilidades.
+            Quanto mais você domina a sua empatia, mais você constrói relações
+            positivas e saudáveis. Tanto no âmbito familiar, quanto
+            profissional, é importante saber se relacionar interpessoalmente.
+            Conseguir transitar entre os grupos, relacionando-se bem com eles, é
+            um dos pilares da Inteligência Emocional. Cultivar relacionamentos
+            baseados no respeito é uma das formas mais eficientes de criar um
+            ambiente positivo ao seu redor.
+          </p>
+          <h2>A mudança depende de você</h2>
+          <p>
+            A maneira como lidamos com nossas questões pessoais e nos
+            relacionamos é determinante para obtermos qualidade de vida. Neste
+            artigo, conhecemos os cinco pilares de Inteligência Emocional, além
+            de como eles se relacionam entre si. Também evidenciamos, a partir
+            dos estudos de Daniel Goleman, que o autoconhecimento é a base da
+            Inteligência Emocional.
+          </p>
+
+          <span className="ref-post">
+          Referência: 
+          <Link to="https://online.pucrs.br/blog/pilares-inteligencia-emocional">Quais são os cinco pilares da Inteligência Emocional? (pucrs.br)
+          </Link>
+          </span>
+        </section>
+    </main>
+  );
+}
